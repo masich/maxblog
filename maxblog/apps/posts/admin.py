d9643c53acb55ae5848b36d7e200d413a04ba08b
Maksym Omelchenko <masich.dev@gmail.com>
2020-02-14 21:13:01 +0200

    Added models for the post app

--- maxblog/apps/posts/admin.py
+++ maxblog/apps/posts/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Post, Comment, Tag, Section
 
-# Register your models here.
+
+# Register models to be available through admin panel
+
+admin.site.register(Post)
+admin.site.register(Comment)
+admin.site.register(Tag)
+admin.site.register(Section)
--- maxblog/apps/posts/models.py
+++ maxblog/apps/posts/models.py
@@ -1,3 +1,41 @@
 from django.db import models
+from django.utils import timezone
+from django.conf import settings
 
-# Create your models here.
+
+class Section(models.Model):
+    """
+    The topic section to which the Post may relate
+    """
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
+
+
+class Tag(models.Model):
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
+
+
+class Post(models.Model):
+    authors = models.ManyToManyField(settings.AUTH_USER_MODEL)
+    title = models.CharField(max_length=100)
+    text = models.TextField()
+    created_date = models.DateTimeField(default=timezone.now)
+    tags = models.ManyToManyField(Tag)
+    section = models.ForeignKey(Section, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
+
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    author_name = models.CharField(max_length=100)
+    text = models.TextField()
+
+    def __str__(self):
+        return self.author_name
