7068161d197acaac375fe429f3616b2e5412d665
Maksym Omelchenko <masich.dev@gmail.com>
2020-02-15 00:19:37 +0200

    Added initial post_details template and view

--- maxblog/apps/posts/urls.py
+++ maxblog/apps/posts/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path, include
 from . import views
 
+app_name = 'posts'
+
 urlpatterns = [
-    path('', views.post_list, name='post_list')
+    path('', views.post_list, name='post_list'),
+    path('post/<int:pk>/', views.post_details, name='post_details'),
 ]
--- maxblog/apps/posts/views.py
+++ maxblog/apps/posts/views.py
@@ -1,6 +1,13 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render, get_object_or_404
 from .models import Post
 
+
 def post_list(request):
-    posts = Post.objects.all().order_by('-created_date')
+    posts = Post.objects.order_by('-created_date')
     return render(request, 'posts/post_list.html', {'posts': posts})
+
+
+def post_details(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    comments = post.comment_set.order_by('-id')
+    return render(request, 'posts/post_details.html', {'post': post, 'comments': comments})
