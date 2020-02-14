423940eee80ea577fa88d47589f3c5f33456c086
Maksym Omelchenko <masich.dev@gmail.com>
2020-02-14 22:57:23 +0200

    Added initial post_list template and view

--- maxblog/apps/posts/views.py
+++ maxblog/apps/posts/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, HttpResponse
-
+from .models import Post
 
 def post_list(request):
-    return HttpResponse()
+    posts = Post.objects.all().order_by('-created_date')
+    return render(request, 'posts/post_list.html', {'posts': posts})
--- maxblog/settings.py
+++ maxblog/settings.py
@@ -39,7 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'maxblog.apps.posts',
+    'posts.apps.PostsConfig',
 ]
 
 MIDDLEWARE = [
@@ -57,7 +57,9 @@ ROOT_URLCONF = 'maxblog.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            os.path.join(PROJECT_ROOT, 'templates'),
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
