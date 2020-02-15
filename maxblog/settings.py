b62fe20fe619cc71276fed253f6b13e5d26b48c3
Maksym Omelchenko <masich.dev@gmail.com>
2020-02-16 00:37:28 +0200

    Added initial docker support

--- maxblog/settings.py
+++ maxblog/settings.py
@@ -84,9 +84,9 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'postgres',
-        'USER': 'postgres',  # default postgres user
-        'HOST': 'localhost',
-        'PORT': 5432,  # default postgres port
+        'USER': 'postgres',
+        'HOST': 'db', # set in docker-compose.yml
+        'PORT': 5432, # default postgres port
     }
 }
 
